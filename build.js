04828d0fc26d36259dd7f7337eb69fbe6678c576
Woody Goodricke <github@andrewgoodricke.com>
2016-07-29 20:24:04 -0400

    Added example build.js. This will be moved to test for unit testing.

---
+++ build.js
@@ -0,0 +1,16 @@
+'use strict';
+
+// var debug_lib   = require('debug');
+// var debug       = debug_lib('nuxeo-build');
+// var error       = debug_lib('nuxeo-build:error');
+var path        = require('path');
+// var yaml_config = require('node-yaml-config');
+var builder     = require('./lib/builder');
+
+// /*"nuxeo-docs": "git@github.com:nuxeo/doc.nuxeo.com.git#separate-content"*/
+// git lfs clone git@github.com:nuxeo/doc.nuxeo.com.git modules/nuxeo-docs && pushd $_ && git checkout -f separate-content && npm install && popd
+// pushd modules/nuxeo-docs && git pull && npm install && popd
+
+var config = {};
+
+builder(config, path.join(__dirname, 'src'));
