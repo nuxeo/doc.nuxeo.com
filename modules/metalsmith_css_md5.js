7ed9a56f5c76f020983c617fd92fdaeec4c75494
Woody Goodricke <github@andrewgoodricke.com>
2016-08-01 21:09:56 -0400

    CSS MD5 support: Added

---
+++ modules/metalsmith_css_md5.js
@@ -0,0 +1,22 @@
+'use strict';
+
+var debug = require('debug')('metalsmith-css-md5');
+var fs = require('fs');
+
+var css_md5 = function (options) {
+
+    return function (files, metalsmith, done) {
+        var metadata = metalsmith.metadata();
+        var stats_filepath = metalsmith.path(options.stats_file);
+
+        debug('CSS MD5 filepath: %s', stats_filepath);
+        var css_md5_string = fs.readFileSync(stats_filepath).toString().split('\n')[0];
+
+        metadata.css = metadata.css || {};
+        metadata.css.md5 = css_md5_string;
+
+        done();
+    };
+};
+
+module.exports = css_md5;
