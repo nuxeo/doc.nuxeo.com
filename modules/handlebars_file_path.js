674d3db72db21f13bf743b25aa0c2bfe33d0609a
Woody Goodricke <github@andrewgoodricke.com>
2016-08-16 19:28:22 -0300

    File Path: Fixed + removed unnecessary excerpts and multiexcerpts helpers

--- modules/handlebars_file_path.js
+++ modules/handlebars_file_path.js
@@ -6,12 +6,22 @@ var slug = require('slug');
 slug.defaults.modes.pretty.lower = true;
 
 var file = function (options) {
-    var url_arr = ['assets'];
-    var space = options.hash.space || options.data.root.hierarchy.space_path;
+    var url_arr = [];
+    if (options.hash.version && options.hash.space) {
+        url_arr.push(options.hash.version);
+        url_arr.push(options.hash.space);
+    }
+    else if (options.hash.space) {
+        url_arr.push(options.hash.space);
+    }
+    else {
+        url_arr.push(options.data.root.url.key.space_path);
+    }
+
     var pagename = options.hash.page || options.data.root.slug;
     var filename = options.hash.name;
-    if (space && pagename && filename) {
-        url_arr.push(slug(space));
+
+    if (url_arr.length && pagename && filename) {
         url_arr.push(slug(pagename));
         url_arr.push(filename);
     }
@@ -19,6 +29,7 @@ var file = function (options) {
         error('Missing space, page or name parameter');
         throw new Error('Missing space or page parameter');
     }
+    url_arr.unshift('assets');
     // TODO: Check file exists
     return '/' + url_arr.join('/');
 };
