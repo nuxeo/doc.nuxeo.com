6d076a582b8e4cb33fefe6bfbedc4f1a2aa9c1e0
Andrew Goodricke <github@andrewgoodricke.com>
2016-08-23 12:49:25 -0300

    Version Assets: Fixed location

--- scripts/copy_site.sh
+++ scripts/copy_site.sh
@@ -2,14 +2,24 @@
 
 REPO_ID="${1}";
 BRANCH="${2}";
+
 SOURCE_ASSETS="./temp/${REPO_ID}/$BRANCH/assets"; # No triling slash to copy the dir name over
 SOURCE_SITE="./temp/${REPO_ID}/$BRANCH/site/";
 TARGET_BASE=./site/;
 
+if [ $BRANCH = 'master' ]; then
+    TARGET_BASE_ASSETS="${TARGET_BASE}";
+else
+    # main version branch, include version for assets
+    SOURCE_ASSETS="./temp/${REPO_ID}/$BRANCH/assets/";
+    TARGET_BASE_ASSETS="${TARGET_BASE}assets/${BRANCH}/";
+fi
+
 if [ -d $SOURCE_SITE -a ! -L $SOURCE_SITE ]; then
     mkdir -p $TARGET_BASE;
     rsync -av $SOURCE_SITE $TARGET_BASE;
-    rsync -av $SOURCE_ASSETS $TARGET_BASE;
+    mkdir -p $TARGET_BASE_ASSETS;
+    rsync -av $SOURCE_ASSETS $TARGET_BASE_ASSETS;
 else
     exit 1;
 fi
