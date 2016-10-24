66236be068675d35cd7fff4952725d274268d34a
Andrew Goodricke <github@andrewgoodricke.com>
2016-10-24 15:07:49 -0300

    Homepage Search: Hides Label when search results visible.

--- client/js/docs.js
+++ client/js/docs.js
@@ -40,6 +40,34 @@ $(document).ready(function () {
         this.setSelectionRange(0, this.value.length);
     });
 
+    // Homepage search - Hiding Label when it has results
+    var $search_area = $('#search-area');
+    if ($search_area.length) {
+        var $input_group_label = $search_area.find('.input-group-label');
+        $search_area.on('click', function () {
+            var $this = $(this);
+            var $input = $this.find('input.gsc-input');
+            $input.focus();
+        })
+        .on('focus', 'input.gsc-input', function () {
+            /* eslint no-invalid-this: 0 */
+            $(this).keyup();
+            this.setSelectionRange(0, this.value.length);
+        })
+        .on('keyup', 'input.gsc-input', function () {
+            setTimeout(function () {
+                if ($search_area.find('.gsc-results').length && !$search_area.find('.gs-result.gs-no-result').length) {
+                    $input_group_label.addClass('closed');
+                }
+                else {
+                    $input_group_label.removeClass('closed');
+                }
+            }, 1e3);
+        });
+
+        $search_area.click();
+    }
+
     // Menu
     menu($);
 });
@@ -55,4 +83,6 @@ $(window).load(function () {
     if (!testAttribute('input', 'autofocus')) {
         $('input[autofocus]').focus();
     }
+
+    $('#search-area').click();
 });
