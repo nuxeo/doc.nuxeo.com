e623fd022d11f36a56de51917db1e2d63e925a43
Andrew Goodricke <github@andrewgoodricke.com>
2017-01-18 14:38:05 +0000

    TOC scrolling: Corrected
    Also: 80 char width amended

--- client/js/toc.js
+++ client/js/toc.js
@@ -1,8 +1,8 @@
 'use strict';
 /* eslint-env browser, jquery */
 
-// var debounce = require('lodash.debounce');
-var throttle = require('lodash.throttle');
+var debounce = require('lodash.debounce');
+// var throttle = require('lodash.throttle');
 
 var initialise_toc = require('./modules/initialise_toc');
 
@@ -17,6 +17,7 @@ var $h_tags = (no_h4) ? $content.filter('.toc').find('h2, h3') : $content.filter
 // var $toc_nav = $toc.find('nav');
 
 var buffer_height = 100;
+var slide_speed = 400;
 var can_scroll_list = true;
 
 var toc_active_position;
@@ -27,44 +28,8 @@ if ($toc.length && $h_tags.length) {
 
     var $toc_list = initialise_toc();
 
-    // $window.on('resize', function () {
-    //     viewport_height = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
-    //     // console.log('viewport height:', viewport_height);
-    // });
-    // $window.resize();
-    //
-    //
-    // // On scroll adjust TOC height
-    // $window.on('scroll', function () {
-    //     var page_position = $document.scrollTop();
-    //     var stuck_at_top = $toc_nav.hasClass('is-stuck') && $toc_nav.hasClass('is-at-top');
-    //     var content_bottom = $content.offset().top + $content.outerHeight(true);
-    //
-    //     // console.log('content_bottom:', content_bottom);
-    //     // console.log('stuck_at_top:', stuck_at_top);
-    //     // console.log('page_position:', page_position);
-    //     // console.log('content_bottom:', content_bottom);
-    //
-    //     var height = '';
-    //     if (!stuck_at_top) {
-    //         // console.log('nav_offset:', $toc_nav.offset().top);
-    //         height = viewport_height + page_position - $toc_nav.offset().top - margin;
-    //     }
-    //     else if (stuck_at_top) {
-    //         var overlap = (page_position + viewport_height) - content_bottom;
-    //         // console.log('overlap:', overlap);
-    //         if (overlap > 0) {
-    //             height = viewport_height - (overlap + (margin * 2));
-    //         }
-    //     }
-    //     // console.log('height:', height);
-    //     $toc_list.height(height);
-    // });
-    // $window.scroll();
-
-
     // On magellan (scroll-spy) change ensure current item is visible.
-    $toc_list.on('update.zf.magellan', throttle(function () {
+    $toc_list.on('update.zf.magellan', debounce(function () {
         var $active_li = $toc_list.find('.active').closest('li');
         if ($active_li.length) {
             var active_position = $active_li.position().top;
@@ -104,31 +69,34 @@ if ($toc.length && $h_tags.length) {
 
                     if (current_lis.length) {
                         var expand_selector = '#' + current_lis.join(', #');
-                        $toc_list.find('.h3, .h4').not(expand_selector).slideUp();
-                        $toc_list.find(expand_selector).slideDown();
+                        $toc_list.find('.h3, .h4').not(expand_selector).slideUp(slide_speed);
+                        $toc_list.find(expand_selector).slideDown(slide_speed);
                     }
                     else {
-                        $toc_list.find('.h3, .h4').slideUp();
+                        $toc_list.find('.h3, .h4').slideUp(slide_speed);
                     }
                 }
 
                 if (can_scroll_list) {
-                    // Total height of list within scroll area
-                    // var list_height = $toc_list[0].scrollHeight;
-                    var scroll_position = $side_menu_container.scrollTop();
-                    toc_active_position = active_position;
+                    setTimeout(function () {
+                        $side_menu_container.stop(true);
+                        // Total height of list within scroll area
+                        // var list_height = $toc_list[0].scrollHeight;
+                        var scroll_position = $side_menu_container.scrollTop();
+                        toc_active_position = $active_li.position().top;
 
-                    // console.log('list_height:', list_height);
-                    // console.log('active_position:', active_position);
-                    // console.log('scroll_position:', $toc_list.scrollTop());
+                        // console.log('list_height:', list_height);
+                        // console.log('active_position:', active_position);
+                        // console.log('scroll_position:', $toc_list.scrollTop());
 
-                    // Scroll to display the current item (20% from top if possible)
-                    var position = (scroll_position + active_position - buffer_height);
-                    position = (position < 0) ? 0 : position;
-                    // console.log('list_position:', position);
-                    $side_menu_container.animate({
-                        scrollTop: position
-                    }, 500);
+                        // Scroll to display the current item (20% from top if possible)
+                        var position = (scroll_position + toc_active_position - buffer_height);
+                        position = (position < 0) ? 0 : position;
+                        // console.log('list_position:', position);
+                        $side_menu_container.animate({
+                            scrollTop: position
+                        }, slide_speed);
+                    }, slide_speed);
                 }
             }
         }
@@ -136,7 +104,7 @@ if ($toc.length && $h_tags.length) {
 
     // Cancel scrolling upon hover
     $side_menu_container.on('mouseenter', function () {
-        $toc_list.stop(true);
+        $side_menu_container.stop(true);
         can_scroll_list = false;
     });
     $side_menu_container.on('mouseleave', function () {
