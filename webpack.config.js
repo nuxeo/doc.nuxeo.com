fd92dc5d3c8b1407fc78c6a3b52e575f0f5503bb
Andrew Goodricke <github@andrewgoodricke.com>
2017-02-01 11:06:41 +0000

    React: Stripped from repository

--- webpack.config.js
+++ webpack.config.js
@@ -56,7 +56,7 @@ module.exports = {
                 exclude: /node_modules/,
                 loader : 'babel',
                 query  : {
-                    presets: ['es2015', 'react']
+                    presets: ['es2015']
                 }
             }
         ]
