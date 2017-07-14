9f8076120462866cff9733d1c96b5e342630d79b
Andrew Goodricke <github@andrewgoodricke.com>
2017-07-14 19:37:51 +0100

    Headings to ignore markup for ids
    Also added appropriate debugging for since

--- modules/handlebars/since.js
+++ modules/handlebars/since.js
@@ -2,7 +2,7 @@
 /* eslint-env es6 */
 
 // Debugging
-const {warn} = require('../debugger')('handlebars-since');
+const {debug, error} = require('../debugger')('handlebars-since');
 
 // npm packages
 const handlebars = require('handlebars');
@@ -24,15 +24,18 @@ const since = (identifier, options) => {
         identifier = '';
     }
     const file = options.data.root;
+    debug(`identifier: ${identifier}`);
 
     if (identifier) {
-        if (file.site.new_version && file.site.new_version === identifier) {
+        const new_version = file.site.new_version && file.site.new_version.toString();
+        debug(`new_version: ${new_version}`);
+        if (new_version && new_version === identifier) {
             // Show new tag if a version's set and matches
             return_text = new_tag;
         }
     }
     else {
-        warn(`No identifier provided for: ${options.data.root.title}`);
+        error(`No identifier provided for: ${options.data.root.title}`);
     }
 
     return return_text;
