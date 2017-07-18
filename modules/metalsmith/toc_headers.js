d3840f28704bacdb431b47b0c3cb0ac063cf9f63
Andrew Goodricke <github@andrewgoodricke.com>
2017-07-18 12:18:46 +0100

    Content headings: ensures unique ids for referencing

--- modules/metalsmith/toc_headers.js
+++ modules/metalsmith/toc_headers.js
@@ -2,7 +2,7 @@
 /* eslint-env es6 */
 
 // Debugging
-const {debug} = require('../debugger')('metalsmith-toc-headers');
+const {debug, warn, error} = require('../debugger')('metalsmith-toc-headers');
 
 // npm packages
 const cheerio = require('cheerio');
@@ -13,25 +13,51 @@ const cheerio = require('cheerio');
  *
  * @return {Function}
 **/
-const toc_headers = function () {
-    return function (files, metalsmith, done) {
-        const filepaths = Object.keys(files).filter((filepath) => {
-            const file = files[filepath];
-            return file.toc;
-        });
+const toc_headers = () => {
+    return (files, metalsmith, done) => {
+        // Get filepaths for files with toc only
+        const filepaths = Object.keys(files).filter(filepath => files[filepath].toc);
 
-        const get_files = function (filepath) {
+        const get_files = (filepath) => {
             debug(`Processing: ${filepath}`);
             const file = files[filepath];
 
             file.toc_items = [];
 
-            const $ = cheerio.load(file.contents.toString());
+            const $ = cheerio.load(file.contents.toString(), {
+                decodeEntities: false
+            });
+
+            // Make ids unique
+            const ids = {};
+            let has_duplicate = false;
+            $('*[id]').each(function () {
+                /* eslint no-invalid-this: 0 */
+                const $this = $(this);
+                const id = $this.attr('id');
+                debug(`Checking id: ${id}`);
+                if (ids[id] !== void 0) {
+                    $this.attr('id', id + '-' + ++ids[id]);
+                    warn(`file: ${filepath} - Heading duplication id: ${id}`);
+                    has_duplicate = true;
+                }
+                else {
+                    ids[id] = 0;
+                }
+            });
+            if (has_duplicate) {
+                error(`Duplicated headings in file: ${filepath}`);
+                file.contents = Buffer.from($.html(), 'utf8');
+            }
+
+
             const titles = ['h2', 'h3'];
             if (!file.toc_no_h4) {
                 titles.push('h4');
             }
             const $titles = $(titles.join(','));
+
+            // function definition for `this`
             $titles.each(function () {
                 /* eslint no-invalid-this: 0 */
                 const $this = $(this);
