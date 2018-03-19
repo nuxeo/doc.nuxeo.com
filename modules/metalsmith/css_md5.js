ee6f5228c9adbaff28929bbf11fe55ccf4d05149
Andrew Goodricke <github@andrewgoodricke.com>
2018-03-19 10:38:58 +0000

    Updated code for css md5

--- modules/metalsmith/css_md5.js
+++ modules/metalsmith/css_md5.js
@@ -1,25 +1,23 @@
 'use strict';
 
-var debug_lib = require('debug');
-var debug = debug_lib('metalsmith-css-md5');
-var fs = require('fs');
+const debug_lib = require('debug');
+const debug = debug_lib('metalsmith-css-md5');
+const fs = require('fs');
 
-var css_md5 = function(options) {
-  return function(files, metalsmith, done) {
-    var metadata = metalsmith.metadata();
-    var stats_filepath = metalsmith.path(options.stats_file);
+const css_md5 = options => (files, metalsmith, done) => {
+  const metadata = metalsmith.metadata();
+  const stats_filepath = metalsmith.path(options.stats_file);
 
-    debug('CSS MD5 filepath: %s', stats_filepath);
-    var css_md5_string = fs
-      .readFileSync(stats_filepath)
-      .toString()
-      .split('\n')[0];
+  debug('CSS MD5 filepath: %s', stats_filepath);
+  const css_md5_string = fs
+    .readFileSync(stats_filepath)
+    .toString()
+    .split('\n')[0];
 
-    metadata.css = metadata.css || {};
-    metadata.css.md5 = css_md5_string;
+  metadata.css = metadata.css || {};
+  metadata.css.md5 = css_md5_string;
 
-    done();
-  };
+  done();
 };
 
 module.exports = css_md5;
