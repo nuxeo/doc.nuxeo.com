acad870b1cc77f8a457957f4f2213352af613ff9
Andrew Goodricke <github@andrewgoodricke.com>
2019-02-27 10:22:42 +0000

    Fixed Doc Asset source being unavailable

--- modules/metalsmith/doc_assets.js
+++ modules/metalsmith/doc_assets.js
@@ -59,28 +59,26 @@ const get_file = (doc, local_path) => {
         // File doesn't match, go get it.
         debug(`Getting file: ${local_path}`);
 
-        return doc
-          .fetchBlob()
-          .then(
-            res => {
-              debug('Downloaded asset via Nuxeo', res);
-              res.body.pipe(fs.createWriteStream(local_path));
-            },
-            err => {
-              if (err && err.response) {
-                return fetch(err.response.url).then(res => {
-                  debug('Downloaded asset via fetch', res);
-                  res.body.pipe(fs.createWriteStream(local_path));
-                });
-              }
+        return doc.fetchBlob().then(
+          res => {
+            debug('Downloaded asset via Nuxeo', res);
+            res.body.pipe(fs.createWriteStream(local_path));
+          },
+          err => {
+            if (err && err.response) {
+              return fetch(err.response.url).then(res => {
+                debug('Downloaded asset via fetch', res);
+                res.body.pipe(fs.createWriteStream(local_path));
+              });
+            } else {
+              debug('No err.response', err);
               return Promise.reject(err);
             }
-          )
-          .catch(err => {
-            error('File save err: ', err);
-          });
+          }
+        );
+      } else {
+        return void 0;
       }
-      return void 0;
     });
 };
 
@@ -109,75 +107,72 @@ const doc_assets = (options = {}) => (files, metalsmith, done) => {
   const nuxeo = new Nuxeo(nuxeo_config);
   const repo = nuxeo.repository();
 
-  const check_file = (filename, selector) =>
-    new Promise((resolve, reject) => {
-      const file = files[filename];
+  const check_file = (filename, selector) => {
+    const file = files[filename];
 
-      const contents = file.contents.toString();
-      const $ = cheerio.load(contents);
+    const contents = file.contents.toString();
+    const $ = cheerio.load(contents);
 
-      const url_promises = [];
+    const url_promises = [];
 
-      $(selector).each((i, el) => {
-        const $el = $(el);
-        const attr = get_attribute(el);
-        const url = $el.attr(attr);
-        if (url && url.startsWith(nx_assets_url_prefix)) {
-          const uid = url.slice(nx_assets_url_prefix.length);
-          debug(`uid: ${uid}`);
+    $(selector).each((i, el) => {
+      const $el = $(el);
+      const attr = get_attribute(el);
+      const url = $el.attr(attr);
+      if (url && url.startsWith(nx_assets_url_prefix)) {
+        const uid = url.slice(nx_assets_url_prefix.length);
+        debug(`uid: ${uid}`);
 
-          const p = retry(repo.fetch.bind(repo), {
-            max_tries: 5,
-            interval: 500,
-            throw_original: true,
-            args: [uid, { schemas: ['dublincore', 'file', 'document_asset'] }]
+        const p = retry(repo.fetch.bind(repo), {
+          max_tries: 5,
+          interval: 500,
+          throw_original: true,
+          args: [uid, { schemas: ['dublincore', 'file', 'document_asset'] }]
+        })
+          .then(doc => {
+            debug('doc:', doc);
+            // if there is no file return
+            if (!doc.properties['file:content']) {
+              error('nx_asset without file:content');
+              return null;
+            }
+
+            // build the right url
+            const ext = /(?:\.([^.]+))?$/.exec(doc.properties['file:content'].name)[1];
+            const asset_type = doc.properties['doc_asset:nature'];
+            const asset_file = `${nx_assets_base}/${uid}-${asset_type}.${ext}`;
+            const href = `/${asset_file}`;
+            debug('ext:', ext);
+            debug('asset_type:', asset_type);
+            debug('asset_file:', asset_file);
+            debug('href:', href);
+
+            // Update the href in the DOM
+            $el.attr(attr, href);
+            const html = $.html();
+            file.contents = Buffer.from(html, 'utf8');
+
+            return get_file(doc, asset_file);
           })
-            .then(doc => {
-              debug('doc:', doc);
-              // if there is no file return
-              if (!doc.properties['file:content']) {
-                error('nx_asset without file:content');
-                return null;
-              }
+          .catch(err => {
+            error('fetch err:', err);
+            return Promise.reject(err);
+          });
 
-              // build the right url
-              const ext = /(?:\.([^.]+))?$/.exec(doc.properties['file:content'].name)[1];
-              const asset_type = doc.properties['doc_asset:nature'];
-              const asset_file = `${nx_assets_base}/${uid}-${asset_type}.${ext}`;
-              const href = `/${asset_file}`;
-              debug('ext:', ext);
-              debug('asset_type:', asset_type);
-              debug('asset_file:', asset_file);
-              debug('href:', href);
-
-              // Update the href in the DOM
-              $el.attr(attr, href);
-              const html = $.html();
-              file.contents = Buffer.from(html, 'utf8');
-
-              return get_file(doc, asset_file);
-            })
-            .catch(err => {
-              error('fetch err:', err);
-              return new Error('fetch err:', err);
-            });
-
-          url_promises.push(p);
-        }
-      });
-
-      Promise.all(url_promises)
-        .then(() => resolve())
-        .catch(reject);
+        url_promises.push(p);
+      }
     });
+
+    return Promise.all(url_promises);
+  };
 
   const selector = Object.keys(element_mapping).join();
   const file_promises = multimatch(Object.keys(files), pattern).map(filename => check_file(filename, selector));
 
   Promise.all(file_promises)
-    .then(() => done())
+    .then(done)
     .catch(err => {
-      error(err);
+      error('file promises error', err);
       return done(err);
     });
 };
