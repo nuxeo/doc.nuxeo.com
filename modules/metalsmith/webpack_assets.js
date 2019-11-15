2464cfbbb104c4e1116167817cfcf6165f01458e
Andrew Goodricke <github@andrewgoodricke.com>
2019-11-15 12:29:52 +0000

    Testing: usage update and coverage improvement

--- modules/metalsmith/webpack_assets.js
+++ modules/metalsmith/webpack_assets.js
@@ -1,24 +1,26 @@
-'use strict';
+// Debugging
+const { debug, error } = require('../debugger')('metalsmith-webpack-assets');
 
-var debug = require('debug')('metalsmith-webpack-assets');
+const webpack_assets = options => (files, metalsmith, done) => {
+  const metadata = metalsmith.metadata();
+  const stats_filepath = metalsmith.path(options.stats_file);
 
-var webpack_assets = function(options) {
-  return function(files, metalsmith, done) {
-    var metadata = metalsmith.metadata();
-    var stats_filepath = metalsmith.path(options.stats_file);
+  debug('Stats filepath: %s', stats_filepath);
+  const stats = require(stats_filepath);
 
-    debug('Stats filepath: %s', stats_filepath);
-    var stats = require(stats_filepath);
+  metadata.js = metadata.js || {};
 
-    metadata.js = metadata.js || {};
-
+  /* istanbul ignore else: Not needed for testing */
+  if (stats.assetsByChunkName) {
     Object.keys(stats.assetsByChunkName).forEach(function(asset) {
       metadata.js[asset] = stats.publicPath + stats.assetsByChunkName[asset];
       debug('Name: %s; Path: %s', asset, metadata.js[asset]);
     });
+  } else {
+    error('assetsByChunkName missing');
+  }
 
-    done();
-  };
+  done();
 };
 
 module.exports = webpack_assets;
--- test/metalsmith_webpack_assets.js
+++ test/metalsmith_webpack_assets.js
@@ -1,13 +1,21 @@
-'use strict';
-/* eslint-env es6 */
+const { test } = require('tap');
+const path = require('path');
+const { assign } = Object;
 
-const test = require('tap').test;
+const webpack_assets_lib = require('../modules/metalsmith/webpack_assets');
 
-const webpack_assets = require('../modules/metalsmith/webpack_assets');
+const ms_path = x => path.join(__dirname, x);
+const ms_base = {
+  path: ms_path,
+  _metadata: {},
+  metadata: function() {
+    return this._metadata;
+  }
+};
 
 test('webpack_assets is a function', assert => {
   // Get typeof string
-  const expected = webpack_assets && {}.toString.call(webpack_assets);
+  const expected = webpack_assets_lib && {}.toString.call(webpack_assets_lib);
 
   assert.isEqual(expected, '[object Function]', 'webpack_assets is a function');
   assert.end();
@@ -15,7 +23,7 @@ test('webpack_assets is a function', assert => {
 
 test('webpack_assets initialisation returns a function', assert => {
   // Get typeof string
-  const initialised = webpack_assets();
+  const initialised = webpack_assets_lib();
   const expected = initialised && {}.toString.call(initialised);
 
   assert.isEqual(
@@ -24,4 +32,65 @@ test('webpack_assets initialisation returns a function', assert => {
     'webpack_assets is a function after initialisation'
   );
   assert.end();
+});
+
+test('webpack_assets errors with missing manifest', assert => {
+  const webpack_assets = webpack_assets_lib({
+    stats_file: 'fixtures/webpack_assets_manifest_missing.json'
+  });
+
+  assert.throws(webpack_assets, void 0, 'Should throw error');
+  assert.end();
+});
+
+test('webpack_assets initialises with an empty object', assert => {
+  // Get typeof string
+  const webpack_assets = webpack_assets_lib({
+    stats_file: 'fixtures/webpack_assets_manifest_empty.json'
+  });
+
+  const actual = assign({}, ms_base);
+
+  const expected = assign({}, actual, {
+    _metadata: {
+      js: {}
+    }
+  });
+
+  webpack_assets({}, actual, err => {
+    assert.ifError(err, 'Has not errored');
+    assert.isEquivalent(actual, expected, 'bundle object not added');
+    assert.end();
+  });
+});
+
+test('webpack_assets populates metadata accordingly', assert => {
+  // Get typeof string
+  const webpack_assets = webpack_assets_lib({
+    stats_file: 'fixtures/webpack_assets_manifest.json'
+  });
+
+  const actual = assign({}, ms_base);
+
+  const expected = assign({}, actual, {
+    _metadata: {
+      js: {
+        docs:
+          'assets/js/73a6b39143936276a53f/docs.73a6b39143936276a53f.bundle.js',
+        search:
+          'assets/js/73a6b39143936276a53f/search.73a6b39143936276a53f.bundle.js',
+        vendor: 'assets/js/73a6b39143936276a53f/vendor.bundle.js'
+      }
+    }
+  });
+
+  webpack_assets({}, actual, err => {
+    assert.ifError(err, 'Has not errored');
+    assert.isEquivalent(
+      actual,
+      expected,
+      'bundle object not populated appropriately'
+    );
+    assert.end();
+  });
 });
