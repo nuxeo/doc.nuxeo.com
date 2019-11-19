d9cc336ef3e506f454ae448490dd0d50e8346049
Andrew Goodricke <github@andrewgoodricke.com>
2019-11-19 17:33:02 +0000

    Fixed layout for overview and sticky menu

--- client/js/modules/left_menu_height.js
+++ client/js/modules/left_menu_height.js
@@ -3,7 +3,7 @@
 
 module.exports = function($) {
   var $window = $(window);
-  var $side_menu_container = $('#side_menu_container');
+  // var $side_menu_container = $('#side_menu_container');
   var $footer = $('#docs_footer');
   var $goto_top = $('#goto_top');
   var footer_pos;
@@ -18,21 +18,20 @@ module.exports = function($) {
     var window_height = $window.height();
 
     var margin_top_value = pos >= menu_height ? 0 : menu_height - pos;
-    var margin_top = margin_top_value + 'px';
-    if (margin_top !== $side_menu_container.css('margin-top')) {
-      // console.log('changed');
-      $side_menu_container.css('margin-top', margin_top);
-    }
+    // var margin_top = margin_top_value + 'px';
+    // if (margin_top !== $side_menu_container.css('margin-top')) {
+    //   // console.log('changed');
+    //   $side_menu_container.css('margin-top', margin_top);
+    // }
 
-    var reduced_height_footer_value =
-      margin_top_value + pos + window_height - footer_pos;
-    var reduced_height =
-      'calc(100vh - ' + Math.max(reduced_height_footer_value, 0) + 'px)';
-    if (reduced_height !== $side_menu_container.css('height')) {
-      $side_menu_container.css('height', reduced_height);
-    }
-    // console.log('pos', pos, margin_top, $side_menu_container.css('margin-top'), reduced_height);
-
+    var reduced_height_footer_value = pos + window_height - footer_pos;
+    // var reduced_height =
+    //   'calc(100vh - ' + Math.max(margin_top_value, 0) + 'px)';
+    // if (reduced_height !== $side_menu_container.css('height')) {
+    //   $side_menu_container.css('height', reduced_height);
+    // }
+    // console.log('pos', pos, margin_top, reduced_height);
+    //
     // console.log('reduced_height_footer_value', reduced_height_footer_value);
     // console.log('margin_top_value', margin_top_value);
     if (reduced_height_footer_value > 0) {
