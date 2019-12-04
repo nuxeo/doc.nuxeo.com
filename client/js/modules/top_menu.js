3376c5da9e65405a17959d82eadcd85a70e08464
Andrew Goodricke <github@andrewgoodricke.com>
2019-12-04 12:09:31 +0000

    What's new notification opens first time and red dot when new items

--- client/js/modules/top_menu.js
+++ client/js/modules/top_menu.js
@@ -17,8 +17,27 @@ module.exports = function($) {
     $html.toggleClass('no-scroll');
   });
 
-  const whats_new_read = localStorage.getItem('whats_new');
-  console.log('whats_new_read', whats_new_read);
+  const $whats_new = $('#whats_new');
+
+  let whats_new_read = localStorage.getItem('whats_new');
+  // console.log('whats_new_read', whats_new_read);
   const whats_new_latest = $('#whats_new_latest').attr('data-date');
-  console.log('whats_new_latest', whats_new_latest);
+  // console.log('whats_new_latest', whats_new_latest);
+
+  if (whats_new_read === null) {
+    $whats_new
+      .addClass('open')
+      .find('.whats-new')
+      .addClass('whats-new--alert');
+  } else if (whats_new_latest && whats_new_read < whats_new_latest) {
+    $whats_new.find('.whats-new').addClass('whats-new--alert');
+  }
+
+  $whats_new.on('click', function() {
+    if (whats_new_latest && whats_new_read !== whats_new_latest) {
+      localStorage.setItem('whats_new', whats_new_latest);
+      whats_new_read = whats_new_latest;
+      $whats_new.find('.whats-new').removeClass('whats-new--alert');
+    }
+  });
 };
